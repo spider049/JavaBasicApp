3fbc08d600d5cfcd76a78e61aa5e303a1d7f8c7c
spideryom <spideryom049@gmail.com>
2017-04-08 18:00:07 +0700

    Class and method

---
+++ src/ExampleClassMethod.java
@@ -0,0 +1,25 @@
+
+
+
+
+public class ExampleClassMethod {
+    public static void main(String[] args) {    // Method Main:
+        
+        int Total = SumTotal(10,20);
+        int Total2 = SumTotal2(1,11,2);
+        System.out.println("Total = "+ Total); // แสดงผลทางหน้าจอ
+        System.out.println("Total = "+ Total2); // แสดงผลทางหน้าจอ
+    }
+    
+    public static int SumTotal(int A, int B){ // Method SumTotal:
+        
+        return A+B; // ส่งค่า A+B กลับไป
+    }
+    
+    public static int SumTotal2(int A, int B, int C){ // Method SumTotal:
+        
+        return A+B+C; // ส่งค่า A+B กลับไป
+    }
+}
+
+
